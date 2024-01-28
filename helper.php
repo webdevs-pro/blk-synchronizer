06b94605b4730ab95660012547afb36ea6166ab3
WebDevs.pro <shock_31337@ukr.net>
2024-01-28 13:45:47 +0200

    2.4.2

--- helper.php
+++ helper.php
@@ -2,13 +2,26 @@
 
 
 function blk_synk_log( $value, $date = '' ) {
-	file_put_contents( BLK_SYNCHRONIZER_PATH . 'logs/synchronizer-' . $date . '.log', date_i18n('Y-m-d H:i:s') . ' ' . $value . "\n" , FILE_APPEND );
+	$log_directory = BLK_SYNCHRONIZER_PATH . 'logs/';
+	if ( ! file_exists( $log_directory ) ) {
+		 mkdir( $log_directory, 0755, true );
+	}
+	file_put_contents( $log_directory . 'synchronizer-' . $date . '.log', date_i18n('Y-m-d H:i:s') . ' ' . $value . "\n", FILE_APPEND );
 }
 
 function blk_debug_log( $value ) {
-	file_put_contents( BLK_SYNCHRONIZER_PATH . 'logs/debug-' . date_i18n('Y-m-d') . '.log', date_i18n('Y-m-d H:i:s') . ' ' . $value . "\n" , FILE_APPEND );
+	$log_directory = BLK_SYNCHRONIZER_PATH . 'logs/';
+	if ( ! file_exists( $log_directory ) ) {
+		 mkdir( $log_directory, 0755, true );
+	}
+	file_put_contents( $log_directory . 'debug-' . date_i18n('Y-m-d') . '.log', date_i18n('Y-m-d H:i:s') . ' ' . $value . "\n", FILE_APPEND );
 }
 
 function blk_error_log( $value ) {
-	file_put_contents( BLK_SYNCHRONIZER_PATH . 'logs/errors-' . date_i18n('Y-m-d') . '.log', date_i18n('Y-m-d H:i:s') . ' ' . $value . "\n" , FILE_APPEND );
+	$log_directory = BLK_SYNCHRONIZER_PATH . 'logs/';
+	if ( ! file_exists( $log_directory ) ) {
+		 mkdir( $log_directory, 0755, true );
+	}
+	file_put_contents( $log_directory . 'errors-' . date_i18n('Y-m-d') . '.log', date_i18n('Y-m-d H:i:s') . ' ' . $value . "\n", FILE_APPEND );
 }
+
